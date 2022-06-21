38fbff16c25445f5cc787450e83215b967a03434
bigjoe003 <josephjunioh@gmail.com>
2022-06-20 17:25:09 -0700

    you

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -14,11 +14,12 @@ char *_strpbrk(char *s, char *accept)
 
 	while (*s)
 	{
-		for (index = 0; accept[index]; index)
+		for (index = 0; accept[index]; index++)
 		{
 			if (*s == accept[index])
 				return (s);
 		}
+
 		s++;
 	}
 	return ('\0');
