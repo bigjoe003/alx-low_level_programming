8ebb3b45621517260f7a43ee7d9a1eccb8be240f
bigjoe003 <josephjunioh@gmail.com>
2022-06-24 19:39:47 -0700

    yyyy

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
  *
  * Return: Always 0.
  */
-int main(int _ _attribute((_ _unused_ _)) argc, char *argv[])
+int main(int __attribute((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 
